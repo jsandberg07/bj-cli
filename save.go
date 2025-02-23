ea8ab5b5a7cb612910af5219b8e510b9ce923a16
Joel Sandberg <jsandberg07@gmail.com>
2025-02-23 17:55:43 -0500

    WAHM

--- save.go
+++ save.go
@@ -6,3 +6,14 @@ import "fmt"
 func Fart() {
 	fmt.Println("Pffrrtt.....")
 }
+
+// things to save
+// name
+// number of bots
+// number of decks
+// goal
+// money
+// stats
+// i dunno everything
+// just give it all a .save() and .load()
+// when you get back from your walk
